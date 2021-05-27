b7202d5eb7c9a96dafd00d1702606110511fcea3
marcos matos <marcoscaiobel@gmai.com>
2021-05-27 17:25:58 -0300

    Adicionar arquivos de projeto.

---
+++ UnitTestContaTempo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestContaTempo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestContaTempo")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61e648ba-e127-4a84-aa9b-1567e4172f4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestContaTempo/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using ContaTempo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestContaTempo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ContaDias contaDias = new ContaDias();
+        [TestMethod]
+        public void DeveRetornarNumeroSeDias()
+        {
+            DateTime dataInformada = new DateTime(2021,05,18);
+            Assert.AreEqual(8, contaDias.DiasPassados(dataInformada));
+        }
+        [TestMethod]
+        public void DeveRetornarNumeroDeSemanas()
+        {
+            DateTime dataInformada = new DateTime(2021, 05, 18);
+            Assert.AreEqual(1, contaDias.SemanasPassados(dataInformada));
+        }
+        [TestMethod]
+        public void DeveRetornarNumeroDeMeses()
+        {
+            DateTime dataInformada = new DateTime(2020, 12, 18);
+            Assert.AreEqual(5, contaDias.MesesPassados(dataInformada));
+        }
+        [TestMethod]
+        public void DeveRetornarNumeroDeAnos()
+        {
+            DateTime dataInformada = new DateTime(2019, 05, 18);
+            Assert.AreEqual(2, contaDias.AnosPassados(dataInformada));
+        }
+       
+        //[TestMethod]
+        //public void DeveRetornarAnosPorEscritoMaisE()
+        //{
+        //    DateTime dataInformada = new DateTime(2019, 05, 18);
+        //    Assert.AreEqual("Dois anos e", contaDias.escreveData(dataInformada));
+        //}
+
+        [TestMethod]
+        public void DeveRetornarDoisAnoseDoisMeses()
+        {
+            DateTime dataInformada = new DateTime(2019, 02, 18);
+            Assert.AreEqual("Dois anos e Dois meses ", contaDias.escreveData(dataInformada));
+        }
+    }
+}
